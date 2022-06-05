7c669923d18938311ed85dfee41ca063f7cd103f
= <=>
2022-06-05 20:59:37 -0300

    adding header form for search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { Field, Form, Formik } from "formik";
+import "./Header.css";
+
 function App() {
   return (
     <div>
-      Hello World
+      <header className="header">
+        <Formik
+          initialValues={{ search: "" }}
+          onSubmit={async (values) => {
+            const response = await fetch(
+              `https://api.unsplash.com/search/photos?per_page=20&&query=${values.search}`,
+              {
+                headers: {
+                  Authorization:
+                    "Client-ID LmCjlb9s29mZTiW7pWlMO5EnfIyxD8jWh212o22s6xY",
+                },
+              }
+            );
+            const data = await response.json();
+          }}
+        >
+          <Form>
+            <Field name="search"></Field>
+          </Form>
+        </Formik>
+      </header>
     </div>
   );
 }
